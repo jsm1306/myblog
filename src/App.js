327dba7144acb97ac509fc833836096e47637a71
jsm1306 <sindhu.j1729@gmail.com>
2024-12-28 21:45:34 +0530

    Experiment-5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
+import Home from './Home';
+import Navbar from './NavBar';
 const myFunction = ()=> {
- const myStr="Hello";
- const a=2.5;
- const myLink="https://www.google.com";
+
  return (
  <div className="App">
+    <Navbar />
  <div className="content">
  <h1>Hello World</h1>
- <h1>{myStr}</h1>
- <h1>{a}</h1>
-<a href = {myLink}> MyLink </a>
+ <Home />
  </div>
  </div>
  );
