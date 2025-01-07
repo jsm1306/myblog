9ae0295acdbe72f7b3981817c6c162d9ec18f0f0
jsm1306 <sindhu.j1729@gmail.com>
2025-01-07 11:48:03 +0530

    Experiment13

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,18 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
-    const title = props.title
+    const title = props.title;
+    const author = props.author;
+    const handleDelete = props.handleDelete;
+    /*   <button onClick={()=>handleDelete(blog.author)}>Delete Blog</button>
+    to delete all the authors.
+*/
     return ( 
         <div className="BlogList">
             <h1>{title}</h1>
             <div className="home">
+            
+        <button onClick={()=>handleDelete(author)}>Delete Blog</button>
+
     {blogs.map((blog) => (
     <div className="blog-preview">
     <h2>{blog.title}</h2>
--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import BlogList from './BlogList';
 const Home = () => {
     const [blogs,setBlogs]=useState([
-    {title:"My Blog1", body:"Body of Blog1", author:"Ramu"},
-    {title:"My Blog2", body:"Body of Blog2", author:"Raju"},
-    {title:"My Blog3", body:"Body of Blog3", author:"Ramu"}
+    {title:"My Blog1", body:"Body of Blog1", author:"Ramu",id:1},
+    {title:"My Blog2", body:"Body of Blog2", author:"Raju",id:2},
+    {title:"My Blog3", body:"Body of Blog3", author:"Ramu",id:3},
     ]);
+    const handleDelete = ()=>{
+        const newblogs = blogs.filter(blog=>blog.author!=="Raju")
+        setBlogs(newblogs)
+    }
     return (
     <div className="Home">
-        <BlogList blogs = {blogs} title = "Hello Sindhu"/>
-        <BlogList blogs = {blogs.filter((blog)=>blog.author==='Ramu')} title = "Hello Ramu Author"/>
+        <BlogList blogs = {blogs} title = "Hello Sindhu" handleDelete={handleDelete}/>
+
     </div>
     );
    }
